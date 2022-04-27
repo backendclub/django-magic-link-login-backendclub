cb7084db938a76c574bc91677b21225da658ee34
Yoong Kang Lim <yoongkang.lim@gmail.com>
2022-04-27 21:25:11 +1000

    Docker-related changes

--- Dockerfile
+++ Dockerfile
@@ -23,4 +23,4 @@ USER appuser
 
 # During debugging, this entry point will be overridden. For more information, please refer to https://aka.ms/vscode-docker-python-debug
 # File wsgi.py was not found. Please enter the Python path to wsgi file.
-CMD ["gunicorn", "--bind", "0.0.0.0:8000", "pythonPath.to.wsgi"]
+CMD ["gunicorn", "--bind", "0.0.0.0:8000", "magiclink.wsgi"]
--- magiclink/settings.py
+++ magiclink/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'magiclink.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME", "testproject"),
+        'USER': os.getenv("DB_USER", "testproject"),
+        'PASSWORD': os.getenv("DB_PASSWORD", "hello"),
+        'HOST': os.getenv("DB_HOST", "postgresdb"),
+        'PORT': os.getenv("DB_PORT", 5432),
+        'CONN_MAX_AGE': 60,
     }
 }
 
